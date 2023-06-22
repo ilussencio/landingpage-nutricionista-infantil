07c08503a085abc90462b408f85979845849c7b3
Rodolfo C Ribeiro <rodolfo.ribeiro@estudante.iftm.edu.br>
2023-06-22 11:02:41 -0300

    commit dos componentes Faq e Quem sou EU

--- src/components/main/section_peguntas/Perguntas.jsx
+++ src/components/main/section_peguntas/Perguntas.jsx
@@ -1,10 +1,37 @@
 import TituloSubtitulo from "../common/TituloSubtitulo"
 import titulosDescricoesSecoes from "../../../js/titulosDescricoesSecoes"
+import {useEffect, useState} from "react";
 
 export default function Perguntas() {
+
+    const [faqs, setFaqs] = useState([])
+
+    useEffect(() => {
+        fetch('faqs.json')
+            .then(file => file.json())
+            .then(conteudo => setFaqs(conteudo.faqs))
+        console.log('useEffect rodou!')
+    }, [])
+
+    function getFaqs() {
+        return faqs.map((faq, id) => {
+            return (
+                <details key={id}>
+                    <summary>{faq.pergunta}</summary>
+                    <p>{faq.resposta}</p>
+                </details>
+            )
+        })
+    }
+
     return (
-        <section>
-            <TituloSubtitulo titulo={titulosDescricoesSecoes["perguntas"].titulo} subtitulo={titulosDescricoesSecoes["perguntas"].subtitulo}/>
+        <section className={"faq"}>
+            <TituloSubtitulo titulo={titulosDescricoesSecoes["perguntas"].titulo}
+                             subtitulo={titulosDescricoesSecoes["perguntas"].subtitulo}/>
+            <div className="faq-detalhes">
+                {getFaqs()}
+            </div>
         </section>
+
     )
 }
