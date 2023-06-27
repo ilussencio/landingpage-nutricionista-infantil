88ac3ab323fbc36c2393686cc3671f4d2fc0f00a
Rodolfo C Ribeiro <rodolfo.ribeiro@estudante.iftm.edu.br>
2023-06-27 18:23:18 -0300

    pequena correção componente Chamada.jsx

--- src/components/main/section_chamada/Chamada.jsx
+++ src/components/main/section_chamada/Chamada.jsx
@@ -4,7 +4,9 @@ import titulosDescricoesSecoes from "../../../js/titulosDescricoesSecoes"
 export default function Chamada() {
     return (
         <section className="chamada" id="chamada">
+            <div className="texto">
             <TituloSubtitulo titulo={titulosDescricoesSecoes["chamada"].titulo} subtitulo={titulosDescricoesSecoes["chamada"].subtitulo}/>
+            </div>
         </section>
     )
 }
