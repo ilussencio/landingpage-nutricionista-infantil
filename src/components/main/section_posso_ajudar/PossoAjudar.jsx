92bdd8290e071ef61001136636da679d77c44c21
ilussencio <ilussencio@gmail.com>
2023-06-28 15:25:10 -0300

    atualização do posso ajudar (responsividade)

--- src/components/main/section_posso_ajudar/PossoAjudar.jsx
+++ src/components/main/section_posso_ajudar/PossoAjudar.jsx
@@ -7,41 +7,42 @@ import possoAjudarCards from "../../../js/possoAjudarCards"
 
 export default function PossoAjudar() {
     return (
-        
+
         <section className="section-posso-ajudar" id="possoajudar">
-            <TituloSubtitulo 
-                titulo={titulosDescricoesSecoes["possoAjudar"].titulo} 
+            <TituloSubtitulo
+                titulo={titulosDescricoesSecoes["possoAjudar"].titulo}
                 subtitulo={titulosDescricoesSecoes["possoAjudar"].subtitulo}
             />
-        <div className="box-containers-posso-ajudar">
-            <div className="container-posso-ajudar">
-                <CardPossoAjudar
-                    titulo={possoAjudarCards["posParto"].titulo}
-                    listaItems={possoAjudarCards["posParto"].listaItems}
-                    color={possoAjudarCards["posParto"].color}
-                />
-                <CardPossoAjudar
-                    titulo={possoAjudarCards["criancas"].titulo}
-                    listaItems={possoAjudarCards["criancas"].listaItems}
-                    color={possoAjudarCards["criancas"].color}
-                />
+            
+            <div className="box-containers-posso-ajudar">
+                <div className="container-posso-ajudar">
+                    <CardPossoAjudar
+                        titulo={possoAjudarCards["posParto"].titulo}
+                        listaItems={possoAjudarCards["posParto"].listaItems}
+                        color={possoAjudarCards["posParto"].color}
+                    />
+                    <CardPossoAjudar
+                        titulo={possoAjudarCards["criancas"].titulo}
+                        listaItems={possoAjudarCards["criancas"].listaItems}
+                        color={possoAjudarCards["criancas"].color}
+                    />
+                </div>
+                <div className="container-posso-ajudar">
+                    <img src={imagemFamilia} alt="" />
+                </div>
+                <div className="container-posso-ajudar">
+                    <CardPossoAjudar
+                        titulo={possoAjudarCards["familias"].titulo}
+                        listaItems={possoAjudarCards["familias"].listaItems}
+                        color={possoAjudarCards["familias"].color}
+                    />
+                    <CardPossoAjudar
+                        titulo={possoAjudarCards["bebes"].titulo}
+                        listaItems={possoAjudarCards["bebes"].listaItems}
+                        color={possoAjudarCards["bebes"].color}
+                    />
+                </div>
             </div>
-            <div className="container-posso-ajudar">
-                <img src={imagemFamilia} alt="" />
-            </div>
-            <div className="container-posso-ajudar">
-                <CardPossoAjudar
-                    titulo={possoAjudarCards["familias"].titulo}
-                    listaItems={possoAjudarCards["familias"].listaItems}
-                    color={possoAjudarCards["familias"].color}
-                />
-                <CardPossoAjudar
-                    titulo={possoAjudarCards["bebes"].titulo}
-                    listaItems={possoAjudarCards["bebes"].listaItems}
-                    color={possoAjudarCards["bebes"].color}
-                />
-            </div>
-        </div>
         </section>
     )
 }
