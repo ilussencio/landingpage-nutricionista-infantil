1e08012df9d8c3f63ee496e47990ae181e8356f1
ilussencio <ilussencio@gmail.com>
2023-07-07 16:17:17 -0300

    sem header

--- src/main.jsx
+++ src/main.jsx
@@ -24,7 +24,7 @@ import './css/personalizada.css'
 
 const router  = createBrowserRouter([
     {path: "/", element: <App />},
-    {path: "/teste", element: <App />},
+    {path: "teste", element: <App />},
     {path: "", element: <App />},
     {errorElement: <ErrorPage />},
     {path: "admin", element: <Admin />},
