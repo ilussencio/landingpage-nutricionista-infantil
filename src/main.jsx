7cf4775d57c5b405d7bc2eedf269e7165fdd1c42
ilussencio <ilussencio@gmail.com>
2023-07-07 15:52:00 -0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -24,6 +24,8 @@ import './css/personalizada.css'
 
 const router  = createBrowserRouter([
     {path: "/", element: <App />},
+    {path: "/teste", element: <App />},
+    {path: "", element: <App />},
     {errorElement: <ErrorPage />},
     {path: "admin", element: <Admin />},
     {path: "home", element: <Home />},
