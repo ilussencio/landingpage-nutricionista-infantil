5602a2414e7619891b0f81955e9258b8fa499124
Rodolfo C Ribeiro <rodolfo.ribeiro@estudante.iftm.edu.br>
2023-07-10 16:00:02 -0300

    aplicado background no component Instafeed.jsx

--- src/components/main/section_instafeed/Instafeed.jsx
+++ src/components/main/section_instafeed/Instafeed.jsx
@@ -1,8 +1,12 @@
+
+
 export default function InstaFeed() {
     return (
-        <section className="instafeed">
-            <figure data-behold-id="iYmdge164D6CSb5WbV0y"></figure>
-        </section>
+        <div className="insta-bckgrd">
+            <div className="instafeed">
+                <figure data-behold-id="iYmdge164D6CSb5WbV0y"></figure>
+            </div>
+        </div>
     )
 
 }
