c73837b2b31e55a04985c4c87afa12dc04446e71
ilussencio <ilussencio@gmail.com>
2023-07-11 22:09:01 -0300

    inserindo texto section instagram

--- src/components/main/section_instafeed/Instafeed.jsx
+++ src/components/main/section_instafeed/Instafeed.jsx
@@ -1,8 +1,10 @@
-
+import TituloSubtitulo from "../common/TituloSubtitulo"
 
 export default function InstaFeed() {
     return (
         <div className="insta-bckgrd">
+            <TituloSubtitulo titulo={"Sabrina no Instagram"} subtitulo={"Para mais conteúdo siga meu perfil: @SabrinaPenido"}/>
+            <br></br>
             <div className="instafeed">
                 <figure data-behold-id="iYmdge164D6CSb5WbV0y"></figure>
             </div>
