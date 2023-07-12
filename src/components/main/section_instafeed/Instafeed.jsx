a8f66c28bf32d67cc16ca00531fe497570291004
Rodolfo C Ribeiro <rodolfo.ribeiro@estudante.iftm.edu.br>
2023-07-12 15:14:45 -0300

    implementa h1 personalizado do componente Instafeed.jsx

--- src/components/main/section_instafeed/Instafeed.jsx
+++ src/components/main/section_instafeed/Instafeed.jsx
@@ -1,9 +1,11 @@
-import TituloSubtitulo from "../common/TituloSubtitulo"
 
 export default function InstaFeed() {
     return (
         <div className="insta-bckgrd" id="instafeed">
-            <TituloSubtitulo titulo={"Sabrina no Instagram"} subtitulo={"Para mais conteúdo siga meu perfil: @SabrinaPenido"}/>
+            <h1 className="frase-instafeed">Sabrina no Instagram   <a href="https://instagram.com/sabrinapenido?igshid=MjAxZDBhZDhlNA==" target="_blank" rel="instagram sabrina">
+                <i className="fa fa-instagram fa-1x" aria-hidden="true"></i>
+            </a></h1>
+
             <br></br>
             <div className="instafeed">
                 <figure data-behold-id="iYmdge164D6CSb5WbV0y"></figure>
